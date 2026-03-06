d3fbe727fc7df79411b61bd2b1ad1d699f1e9d46
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-03-06 15:14:01 +0000

    feat: add createMessageValidators to '/new' POST route
    - run createMessageValidators before createMessage handler

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const indexRouter = Router();
-const { getMessageById, getAddMessagePage, getIndexPage, createMessage } = require("../controllers/indexController");
+const { getMessageById, getAddMessagePage, getIndexPage, createMessage, createMessageValidators } = require("../controllers/indexController");
 
 const messages = [];
 let messageId = 0;
@@ -9,6 +9,6 @@ indexRouter.get("/", (req, res) => getIndexPage(req, res, messages));
 indexRouter.get("/new", getAddMessagePage);
 indexRouter.get("/message/:id", (req, res) => getMessageById(req, res, messages));
 
-indexRouter.post("/new", (req, res) => createMessage(req, res, messages, messageId));
+indexRouter.post("/new", createMessageValidators, (req, res) => createMessage(req, res, messages, messageId));
 
 module.exports = indexRouter;
